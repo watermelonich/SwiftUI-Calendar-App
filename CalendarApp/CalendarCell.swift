ae866db4354fcd90bc0c76784253bf726deae6c4
Nich <128910037+watermelonich@users.noreply.github.com>
2024-02-23 09:04:18 +0100

    Initial commit

---
+++ CalendarApp/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  CalendarExampleTutorial
+//
+//  Created by Nich on 14/11/23
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+	@IBOutlet weak var dayOfMonth: UILabel!
+}
